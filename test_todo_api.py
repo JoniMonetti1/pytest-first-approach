0e239f024777cd54e5a1862ac549a3e7fc851bbc
Jonathan Monetti <jonathan_monetti@epam.com>
2025-07-01 11:21:14 -0300

    Add tests for task management and refactor task payload generation

--- test_todo_api.py
+++ test_todo_api.py
@@ -1,14 +1,7 @@
 import requests
+import uuid
 
 ENDPOINT = "https://todo.pixegami.io/"
-
-response = requests.get(ENDPOINT)
-print(response)
-
-data = response.json()
-status_code = response.status_code
-print(status_code)
-print(data)
 
 def test_can_call_endpoint():
     response = requests.get(ENDPOINT)
@@ -23,7 +16,6 @@ def test_can_create_task():
     }
     create_task_response = requests.put(ENDPOINT + "/create-task", json=payload)
     data = create_task_response.json()
-    print(data)
 
     assert create_task_response.status_code == 200
 
@@ -32,7 +24,6 @@ def test_can_create_task():
 
     assert get_task_response.status_code == 200
     task_data = get_task_response.json()
-    print(task_data)
 
     assert task_data["content"] == payload["content"]
     assert task_data["user_id"] == payload["user_id"]
@@ -64,6 +55,34 @@ def test_can_update_task():
     assert get_task_data["content"] == update_payload["content"]
     assert get_task_data["is_done"] == update_payload["is_done"]
 
+def test_can_list_tasks():
+    #Crete n tasks
+    n = 5
+    payload = new_task_payload()
+    for _ in range(n):
+        create_task_response = create_task(payload)
+        assert create_task_response.status_code == 200
+
+    # List tasks
+    user_id = payload["user_id"]
+    list_tasks_response = list_tasks(user_id)
+    assert list_tasks_response.status_code == 200
+    data = list_tasks_response.json()
+    assert len(data["tasks"]) == n
+
+def test_can_delete_task():
+    payload = new_task_payload()
+    create_task_response = create_task(payload)
+    assert create_task_response.status_code == 200
+    task_id = create_task_response.json()["task"]["task_id"]
+
+    delete_task_response = delete_task(task_id)
+    assert delete_task_response.status_code == 200
+
+    get_task_response = get_task(task_id)
+    assert get_task_response.status_code == 404, "Task should be deleted and not found"
+
+
 
 
 def create_task(payload: dict):
@@ -72,13 +91,20 @@ def create_task(payload: dict):
 def get_task(task_id: str):
     return requests.get(ENDPOINT + f"/get-task/{task_id}")
 
+def list_tasks(user_id: str):
+    return requests.get(ENDPOINT + f"/list-tasks/{user_id}")
+
 def update_task(payload: dict):
     return requests.put(ENDPOINT + f"/update-task", json=payload)
 
+def delete_task(task_id: str):
+    return requests.delete(ENDPOINT + f"/delete-task/{task_id}")
+
 def new_task_payload():
+    user_id = f"test_user_{uuid.uuid4().hex}"
+    content = f"test_content_{uuid.uuid4().hex}"
     return {
-        "content": "Buy groceries and cook dinner",
-        "user_id": "user_12345",
-        "task_id": "task_67890",
+        "content": content,
+        "user_id": user_id,
         "is_done": False,
     }
