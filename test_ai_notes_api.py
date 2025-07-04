eef5bafc8c4aa82617b890216bc06ef219b5d1d0
Jonathan Monetti <jonathan_monetti@epam.com>
2025-07-04 12:24:10 -0300

    Add test for updating user information

--- test_ai_notes_api.py
+++ test_ai_notes_api.py
@@ -57,6 +57,30 @@ def test_can_create_user(auth_token):
         assert user['username'] == payload['username']
         assert user['email'] == payload['email']
 
+def test_can_update_user(auth_token):
+    headers = {
+        'Authorization': 'Bearer ' + auth_token,
+        'Content-Type': 'application/json'
+    }
+
+    payload = new_user_payload()
+    create_response = create_user(payload, headers)
+    assert create_response.status_code == 201
+    username = create_response.json()['username']
+
+    user_id = get_user_by_username(username, headers = {
+        'Authorization': 'Bearer ' + auth_token
+    }).json()['id']
+
+    updated_payload = {
+        "email": f"{username}test@gmail.com",
+    }
+
+    update_user_response = update_user(updated_payload, user_id, headers)
+    assert update_user_response.status_code == 202
+    updated_user = update_user_response.json()
+    assert updated_user['email'] == updated_payload['email']
+
 
 
 def new_user_payload():
@@ -70,8 +94,14 @@ def new_user_payload():
 def create_user(payload: dict, headers: dict):
     return requests.post(BASE_URL + "/user", headers=headers, json=payload)
 
+def update_user(payload: dict, user_id: int, headers: dict):
+    return requests.put(BASE_URL + f"/user/{user_id}", headers=headers, json=payload)
+
 def get_user(user_id: int, headers: dict):
     return requests.get(BASE_URL + f"/user/{user_id}", headers=headers)
+
+def get_user_by_username(username: str, headers: dict):
+    return requests.get(BASE_URL + f"/user/username/{username}", headers=headers)
 
 def get_users(headers: dict):
     return requests.get(BASE_URL + "/user", headers=headers)
